a141a688602bac46b892c795f27ab16b06f5c17d
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-11-21 22:03:16 +0300

    my project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+using System;
+using System.Runtime.Intrinsics.Arm;
+
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine( "Введите число first: ");
+        int first = Convert.ToInt32(Console.ReadLine());
+
+        if (first % 7 ==0 && first % 23 == 0)
+        {
+        System.Console.WriteLine("Верно");
+    }
+    else
+    {
+        System.Console.WriteLine ("Неверно");
+    }
+}
+}
