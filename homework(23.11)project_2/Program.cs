000e58aca38044bc4e6ce8879955437211ca4d49
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-11-23 15:37:04 +0300

    homework project 2

---
+++ homework(23.11)project_2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Внутри класса Answer напишите метод FindMax, который принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+using System;
+
+public class Answer {
+    static int FindMax(int a, int b, int c)
+    {
+      int max = a;
+        if (max < b)
+        {
+            max = b;
+        }
+        if (max < c)
+        {
+            max = c;
+        }
+        return max;
+    }
+
+    static public void Main(string[] args) {
+        int a, b, c;
+
+        if (args.Length >= 3) {
+            a = int.Parse(args[0]);
+            b = int.Parse(args[1]);
+            c = int.Parse(args[2]);
+        } else {
+            a = 5;
+            b = 6;
+            c = 7;
+        }
+
+        int result = FindMax(a, b, c);
+        System.Console.WriteLine($"{result}");
+    }
+}
